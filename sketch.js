0ddc1953b2d35700735fa76f95341a29dfc30e89
Sam Merrick <samjmerrick@gmail.com>
2023-06-28 15:32:39 +0100

    Add button to change grid size

--- sketch.js
+++ sketch.js
@@ -1,20 +1,41 @@
-const gridSize = 8;
+let gridSize = 8;
 const sketch = document.querySelector(".sketch");
 
-sketch.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
-sketch.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
+setGrid();
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-  const pixel = document.createElement("div");
-  pixel.className = "pixel";
-  sketch.appendChild(pixel);
+function changeGrid() {
+  let size = prompt("How large should the grid be?");
+  size = parseInt(size);
+
+  if(!Number.isInteger(size)|| size > 100 || size < 2) {
+    alert('Please enter a number between 2-100')
+    return;
+  }
+  gridSize = size;
+  setGrid();
 }
 
-document.querySelectorAll('.pixel').forEach(pixel => {
-  pixel.addEventListener('mousedown', () => pixel.classList.toggle('active'));
-  pixel.addEventListener('mouseover', (e) => {
-    if(e.buttons == 1) {
-      pixel.classList.toggle('active');
-    }
+function setGrid() {
+
+  while(sketch.firstChild) {
+    sketch.firstChild.remove();
+  }
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const pixel = document.createElement("div");
+    pixel.className = "pixel";
+    sketch.appendChild(pixel);
+  }
+
+  sketch.style.gridTemplateColumns = `repeat(${gridSize}, 1fr`;
+  sketch.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
+
+  document.querySelectorAll('.pixel').forEach(pixel => {
+    pixel.addEventListener('mousedown', () => pixel.classList.toggle('active'));
+    pixel.addEventListener('mouseover', (e) => {
+      if(e.buttons == 1) {
+        pixel.classList.toggle('active');
+      }
+    });
   });
-});
+}
